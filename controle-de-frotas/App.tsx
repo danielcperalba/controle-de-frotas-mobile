10f44caf3933618a046e8a6c5ef8552954cb92b1
Daniel Peralba <contatodanielperalba@hotmail.com>
2024-03-23 11:11:28 -0300

    primeiros passos com estilização

--- controle-de-frotas/App.tsx
+++ controle-de-frotas/App.tsx
@@ -1,8 +1,36 @@
 import React from "react";
-import { Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function App(){
     return(
-        <Text>Hell World!</Text>
+        <View style={styles.container}>
+            <Text style={styles.eventName}>
+                Gastos
+            </Text>
+
+            <Text style={styles.eventDescribe}>
+                Adicione os gastos que você teve durante a viagem
+            </Text>
+        </View>
+        
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        padding:24
+    },
+
+    eventName: {
+        color: '#FDFCFE',
+        fontSize: 28,
+        fontWeight: 'bold',
+        marginTop: 48
+    },
+
+    eventDescribe:{
+        color: '#6B6B6B',
+        fontSize: 12
+    }
+});
