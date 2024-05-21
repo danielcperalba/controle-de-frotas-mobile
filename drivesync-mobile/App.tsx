64803c63e7e622bb1ee163f3e041426575d9a011
Daniel Campos <contatodanielperalba@hotmail.com>
2024-05-21 20:45:31 -0300

    criando variaveis de ambiente

--- drivesync-mobile/App.tsx
+++ drivesync-mobile/App.tsx
@@ -1,10 +1,12 @@
-import { SignIn } from "./src/screens/SignIn";
 import { ThemeProvider } from "styled-components";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 
 import theme from './src/theme';
+
+import { SignIn } from "./src/screens/SignIn";
 import { Loading } from "./src/components/Loading";
 import { StatusBar } from "react-native";
+
 
 export default function App(){
   const[fontsLoaded] = useFonts ({Roboto_400Regular, Roboto_700Bold});
