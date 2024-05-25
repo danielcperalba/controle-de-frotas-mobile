73b4f26526725611b1c32a139a90fdfdedbb11ba
Daniel Peralba <daniel.peralba@hotmail.com>
2024-05-25 13:44:08 -0300

    ajustes de layout

--- drivesync-mobile/src/components/VeiculoCard/styles.ts
+++ drivesync-mobile/src/components/VeiculoCard/styles.ts
@@ -34,6 +34,7 @@ const styles = StyleSheet.create({
   },
   content: {
     flex: 1,
+    gap: 2
   },
   row: {
     flexDirection: "row",
--- drivesync-mobile/src/screens/Veiculos/index.tsx
+++ drivesync-mobile/src/screens/Veiculos/index.tsx
@@ -23,6 +23,7 @@ export default function Veiculo() {
         <VeiculoCard />
         <VeiculoCard />
         <VeiculoCard />
+
         
       </View>
     </View>
