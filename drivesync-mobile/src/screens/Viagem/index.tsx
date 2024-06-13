1d038308d9c2f3a96d96c0b7bd109404ecec0b98
Daniel Campos <contatodanielperalba@hotmail.com>
2024-06-13 10:42:59 -0300

    Implementando design da tela 'Nova Viagem'

--- drivesync-mobile/src/screens/Viagem/index.tsx
+++ drivesync-mobile/src/screens/Viagem/index.tsx
@@ -1,22 +1,105 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TextInput, ScrollView, TouchableOpacity } from "react-native";
+import CheckBox from 'react-native-check-box';
+import styles from './styles';
 
-export default function Veiculo(){
-    return(
-        <View style={styles.container}>
-        </View>
+export default function Atividades() {
+    const [checkList, setCheckList] = useState({
+        brakes: false,
+        tires: false,
+        lights: false,
+        fuel: false,
+    });
+
+    const handleCheckBoxChange = (item) => {
+        setCheckList({ ...checkList, [item]: !checkList[item] });
+    };
+
+    return (
+        <ScrollView style={styles.container}>
+            <Text style={styles.subtitle}>Informações da viagem</Text>
+
+            <View style={styles.inputsContainer}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Localização atual"
+                    placeholderTextColor="#aaa"
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder="Veículo"
+                    placeholderTextColor="#aaa"
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder="Passageiros"
+                    placeholderTextColor="#aaa"
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder="Quilometragem atual"
+                    placeholderTextColor="#aaa"
+                />
+            </View>
+
+            <View style={styles.divider} />
+
+            <Text style={styles.subtitle}>Check-list</Text>
+            <View style={styles.checkListContainer}>
+                <View style={styles.checkListItem}>
+                    <Text style={styles.checkListText}>Freios</Text>
+                    <CheckBox
+                        style={styles.checkBox}
+                        checkBoxColor='#00875F'
+                        isChecked={checkList.brakes}
+                        onClick={() => handleCheckBoxChange('brakes')}
+                    />
+                </View>
+                <View style={styles.checkListItem}>
+                    <Text style={styles.checkListText}>Pneus</Text>
+                    <CheckBox
+                        style={styles.checkBox}
+                        checkBoxColor='#00875F'
+                        isChecked={checkList.tires}
+                        onClick={() => handleCheckBoxChange('tires')}
+                    />
+                </View>
+                <View style={styles.checkListItem}>
+                    <Text style={styles.checkListText}>Luzes</Text>
+                    <CheckBox
+                        style={styles.checkBox}
+                        checkBoxColor='#00875F'
+                        isChecked={checkList.lights}
+                        onClick={() => handleCheckBoxChange('lights')}
+                    />
+                </View>
+                <View style={styles.checkListItem}>
+                    <Text style={styles.checkListText}>Combustível</Text>
+                    <CheckBox
+                        style={styles.checkBox}
+                        checkBoxColor='#00875F'
+                        isChecked={checkList.fuel}
+                        onClick={() => handleCheckBoxChange('fuel')}
+                    />
+                </View>
+            </View>
+
+            <View style={styles.divider} />
+
+            <Text style={styles.subtitle}>Observações</Text>
+                <TextInput
+                    editable
+                    multiline
+                    numberOfLines={4}
+                    maxLength={40}
+                    style={styles.obsInput}
+                    placeholder="Digite suas observações aqui"
+                    placeholderTextColor="#aaa"
+                />
+
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>Iniciar</Text>
+                </TouchableOpacity>
+        </ScrollView>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#202024'
-    },
-    text:{
-        fontSize: 25,
-        fontWeight: 'bold'
-    }
-});
